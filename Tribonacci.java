69d39ac1378422cb45ea913eb194388f1b8e138c
arnol <arnol01233@gmail.com>
2020-06-25 11:41:36 -0400

    first commit

---
+++ Tribonacci.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Tribonacci here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Tribonacci
+{
+    public double[] tribonacci(double[] s, int n) {
+        return null;
+    }
+}
